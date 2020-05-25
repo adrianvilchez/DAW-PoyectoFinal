9a7d66a60ec5cefcbd6c460e9c5edaf4052c299f
adrianvilchez <avilchez758@gmail.com>
2020-05-25 22:44:08 +0200

    limpiando comentarios innecesarios

--- php/controller/GenerosC.php
+++ php/controller/GenerosC.php
@@ -1,9 +1,9 @@
 <?php
     class GenerosController {
-        
+
         public function obtenerGeneros() {
             include(dirname(__FILE__).'./../model/GenerosM.php');
-            //include_once('../model/GruposM.php');
+
             $modeloGeneros = new GenerosModel();
             
             $generos = $modeloGeneros->obtenerGeneros();
