f68bba618e4bc843e5c3462d6355eb7b553dcef4
adrianvilchez <avilchez758@gmail.com>
2020-06-17 11:19:15 +0200

    implementando copyright e iconos RRSS en el footer

--- php/vista/componentes/Footer.php
+++ php/vista/componentes/Footer.php
@@ -1,7 +1,20 @@
             </main>
             
             <footer>
-                <h1>Esto es un footer</h1>
+                <div class="copyright">
+                    <h1>©YourBand</h1>
+                </div>
+                <div class="rrss">
+                    <div class="facebook">
+                        <a href="https:\\www.facebook.com" target="blank_"><img src="img/facebook.png" alt=""></a>
+                    </div>
+                    <div class="instagram">
+                        <a href="https:\\www.instagram.com" target="blank_"><img src="img/instagram.png" alt=""></a>
+                    </div>
+                    <div class="youtube">
+                        <a href="https:\\www.youtube.com" target="blank_"><img src="img/youtube.png" alt=""></a>
+                    </div>
+                </div>
             </footer>
         </div>
     </body>
